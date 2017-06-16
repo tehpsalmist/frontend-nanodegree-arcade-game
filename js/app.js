757f797363f08e97a3dc470c1c140383fa4525d7
tehpsalmist <ronyfan102@gmail.com>
2017-06-15 22:53:20 -0400

    Initial edits to enable display
    I got the background to show up! yay! and my player now moves around
    the screen! yay!

--- js/app.js
+++ js/app.js
@@ -25,13 +25,48 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 202;
+    this.y = 400;
+}
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key){
+    if(key === 'left' && this.x > 5){
+        this.x -=100;
+        }
+    if(key === 'up' && this.y > 0){
+        this.y -= 83;
+        }
+    if(key === 'right' && this.x < 400){
+        this.x += 100;
+        }
+    if(key === 'down' && this.y < 400){
+        this.y +=83;
+        }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+var player = new Player();
 
-
+var enemyFast = new Enemy();
+var enemySlow = new Enemy();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
